b2329d96424f650ad78efaf7e0923d0041bd14c7
Timo De Waele <timo.dewaele@UGent.be>
2017-12-05 12:19:05 +0100

    day 5

--- day_5/jumps.cpp
+++ day_5/jumps.cpp
@@ -11,11 +11,11 @@ int main() {
         while (getline(file, line)) {
             jumps.push_back(atoi(line.c_str()));
         }
-        int64_t pc = 0;
+        uint64_t pc = 0;
         uint64_t nr_of_steps = 0;
-        while (pc >= 0 && pc < jumps.size()) {
+        while (pc < jumps.size()) {
             int64_t offset = jumps[pc];
-            int64_t old_pc = pc;
+            uint64_t old_pc = pc;
             pc += offset;
             if (offset < 3) {
                 offset++;
