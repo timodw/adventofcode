621cd27f607c3c7ef8c49acd55e2d9fbbd680352
Timo De Waele <timo.dewaele@UGent.be>
2017-12-08 21:37:24 +0100

    day 7: damn this is ugly

--- day_7/recursive_circus.cpp
+++ day_7/recursive_circus.cpp
@@ -3,6 +3,7 @@
 #include <string>
 #include <unordered_set>
 #include <unordered_map>
+#include <vector>
 
 
 class tree_node {
@@ -13,8 +14,11 @@ class tree_node {
         tree_node* parent = nullptr;
         tree_node(std::string n, int w) {name = n; weight = w;};
         tree_node(std::string n) {name = n;};
+        int get_weight();
+        std::pair<bool, tree_node*> get_different_child();
 };
 
+int get_difference();
 void process_line(std::string);
 void add_child(tree_node*, std::string);
 
@@ -28,9 +32,28 @@ int main() {
         while (getline(file, line)) {
             process_line(line);
         }
-        std::cout << "SOLUTION: " << root->name;
+        std::cout << "SOLUTION: " << get_difference();
     }
     return 0;
+}
+
+int get_difference() {
+    std::pair<bool, tree_node*> ret_val = root->get_different_child();
+    while (ret_val.first) {
+        std::pair<bool, tree_node*> tmp = ret_val.second->get_different_child();
+        if (!tmp.first) break;
+        ret_val = tmp;
+    }
+
+    tree_node* other_child = nullptr;
+    for (tree_node* node: ret_val.second->parent->children) {
+        if (node != ret_val.second) {
+            other_child = node;
+            break;
+        }
+    }
+
+    return ret_val.second->weight - (ret_val.second->get_weight() - other_child->get_weight());
 }
 
 void process_line(std::string line) {
@@ -72,12 +95,39 @@ void process_line(std::string line) {
 void add_child(tree_node* parent, std::string child_name) {
     if (nodes.find(child_name) != nodes.end()) {
         //child already in map
-        nodes[child_name]->parent = parent; 
+        nodes[child_name]->parent = parent;
     } else {
-        tree_node* new_node = new tree_node(child_name); 
+        tree_node* new_node = new tree_node(child_name);
         new_node->parent = parent;
-        nodes[child_name] = new_node; 
+        nodes[child_name] = new_node;
     }
     parent->children.insert(nodes[child_name]);
     if (nodes[child_name] == root) root = parent;
+}
+
+int tree_node::get_weight() {
+    if (children.size() != 0) {
+        int sum = weight;
+        for (tree_node* node: children) {
+            sum += node->get_weight();
+        }
+        return sum;
+    } else {
+        return weight;
+    }
+}
+
+std::pair <bool, tree_node*> tree_node::get_different_child() {
+    bool different = false;
+    std::vector<tree_node*> vec(children.begin(), children.end());
+    tree_node* diff_child = vec[0];
+    for (size_t i = 1; i < vec.size(); i++) {
+        if (diff_child->get_weight() != vec[i]->get_weight() &&
+                vec[i]->get_weight() != vec[(i + 1) % vec.size()]->get_weight()) {
+            different = true;
+            diff_child = vec[i];
+            break;
+        }
+    }
+    return std::pair<bool, tree_node*>(different, diff_child);
 }
